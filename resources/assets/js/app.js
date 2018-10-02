68329d16712e3f7b4dd9b46b7892e83f222f8c60
vagrant <vagrant@homestead>
2018-10-02 05:08:47 +0000

    新增收货地址

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,9 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+ //此处需在引入vue之后引入
+require('./components/SelectDistrict');
+require('./components/UserAddressesCreateAndEdit');
 
 const app = new Vue({
     el: '#app'
