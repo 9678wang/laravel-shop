9d713213ba4e172219b3bca56c42abb06e83d20d
vagrant <vagrant@homestead>
2018-10-03 11:47:03 +0000

    完善支付后逻辑

--- app/Listeners/SendOrderPaidMail.php
+++ app/Listeners/SendOrderPaidMail.php
@@ -3,6 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\OrderPaid;
+use App\Notifications\OrderPaidNotification;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -27,7 +28,7 @@ class SendOrderPaidMail implements ShouldQueue
     public function handle(OrderPaid $event)
     {
         //从事件对象中取出对应的订单
-        $order = $evnet->getOrder();
+        $order = $event->getOrder();
         //调用notify方法来发送通知
         $order->user->notify(new OrderPaidNotification($order));
     }
