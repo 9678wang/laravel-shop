67a1ea44297d44e8b886698ceeb1ede1cef993d2
vagrant <vagrant@homestead>
2018-10-04 03:33:00 +0000

    用户界面 - 使用优惠券下单

---
+++ app/Exceptions/CouponCodeUnavailableException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Http\Request;
+use Exception;
+
+class CouponCodeUnavailableException extends Exception
+{
+    public function __construct($message, int $code = 403)
+    {
+    	parent::__construct($message, $code);
+    }
+
+    //当这个异常被触发时，会调用render方法来输出给用户
+    public function render(Request $request)
+    {
+    	//如果用户通过Api请求，则返回json格式的错误信息
+    	if($request->expectsJson()){
+    		return response()->json(['msg' => $this->message], $this->code);
+    	}
+
+    	//否则返回上一页并带上错误信息
+    	return redirect()->back()->witchErrors(['coupon_code' => $this->message]);
+    }
+}
