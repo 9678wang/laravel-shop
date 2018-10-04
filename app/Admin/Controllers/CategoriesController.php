dddc007a0a3b195371918e65de39d84e93e50683
vagrant <vagrant@homestead>
2018-10-04 08:15:02 +0000

    商品后台添加类目字段

--- app/Admin/Controllers/CategoriesController.php
+++ app/Admin/Controllers/CategoriesController.php
@@ -158,7 +158,7 @@ class CategoriesController extends Controller
         //用户输入的值通过q参数获取
         $search = $request->input('q');
         $result = Category::query()
-            ->where('is_directory', true)//由于这里选择的是父类目，因此需要限定is_directory为true
+            ->where('is_directory', boolval($request->input('is_directory', true)))
             ->where('name', 'like', '%'.$search.'%')
             ->paginate();
 
