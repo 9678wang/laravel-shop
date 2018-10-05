8f56f6c56dd1312eb56f986db8397ed574068a45
vagrant <vagrant@homestead>
2018-10-05 07:35:31 +0000

    微信支付分期还款

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,10 @@ Route::group(['middleware' => 'auth'], function(){
 		Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
 		Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
 		Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
+		Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
 	});
 });
+Route::post('installments/wechat/notify', 'InstallmentsController@wechatNotify')->name('installments.wechat.notify');
 Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
