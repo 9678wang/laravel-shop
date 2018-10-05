d3c41532b14f83b4179d14cd5dad22707f4e3c87
vagrant <vagrant@homestead>
2018-10-05 03:59:38 +0000

    初始化分期付款模型

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -12,3 +12,9 @@ function ngrok_url($routeName, $parameters = [])
 		return $url.route($routeName, $parameters, false);
 	}
 }
+
+//默认的精度为小数点后两位
+function big_number($number, $scale = 2)
+{
+	return new \Moontoast\Math\BigNumber($number, $scale);
+}
