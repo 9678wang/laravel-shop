eae91bb9be047e3273b94d65ed60e389e0b88da2
vagrant <vagrant@homestead>
2018-10-06 10:40:32 +0000

    秒杀商品详情页

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['middleware' => 'auth'], function(){
 		Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
 		Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
 		Route::get('installments/{installment}/wechat', 'InstallmentsController@payByWechat')->name('installments.wechat');
+		Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 	});
 });
 Route::post('installments/wechat/refund_notify', 'InstallmentsController@wechatRefundNotify')->name('installments.wechat.refund_notify');
